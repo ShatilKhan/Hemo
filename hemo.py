a4bbca449f89d53d0b06c8dfa705c2186a7038e3
ShatilKhan <shatilshahriar009@gmail.com>
2024-01-31 23:18:09 +0600

    Add Geo location
    User can submit their current location & see nearby healthcare centers where they can donate blood

--- hemo.py
+++ hemo.py
@@ -1,18 +1,68 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import requests
 from langchain import PromptTemplate
 import seaborn as sns
 from transformers import pipeline
 import pytesseract
 from PIL import Image
+from st_paywall import add_auth
+import os
+from dotenv import load_dotenv
+
+
+
+st.set_page_config(page_title="💉 Hemo")
+st.title('💉 Hemo')
+
+# Auth 
+# add_auth(required=False)
+
+#after authentication, the email and subscription status is stored in session state
+# st.write(st.session_state.email)
+# st.write(st.session_state.user_subscribed)
 
 # Set the path to the Tesseract OCR executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\msys64\mingw64\bin\tesseract.exe' 
 
-st.set_page_config(page_title="💉 Hemo")
-st.title('💉 Hemo')
+# Google Places API URL
+url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+# Load environment variables from .env file
+load_dotenv()
+
+
+
+# Create a text input field for the location
+location = st.text_input("Enter your current location:")
+
+# Search & Display nearby clinics
+if location:
+    # Parameters for the API request
+    params = {
+        "location": location,  # Location name
+        "radius": 5000,  # Search radius in meters
+        "type": "hospital",  # Type of place to search for
+        "key": "AIzaSyDzScf7h3LKKQv552k_C3OJWpCXmfkLPeA" ,  # Google Places API key
+    }
+
+    # Send the API request
+    response = requests.get(url, params=params)
+
+    # Parse the response
+    data = response.json()
+
+    # Print the response data
+    st.write(data)
+
+    # Extract the clinic locations from the response
+    clinics = pd.DataFrame([{
+        'lat': result['geometry']['location']['lat'],
+        'lon': result['geometry']['location']['lng'],
+    } for result in data['results']])
+
+    # Display the clinics on a map
+    st.map(clinics)
 
 # CSV file uploader in sidebar
 uploaded_file = st.sidebar.file_uploader("Upload Blood Donor Dataset", type="csv")
@@ -23,6 +73,8 @@ uploaded_image = st.sidebar.file_uploader("Upload Image", type=["png", "jpg", "j
 # Initialize the pipeline
 pipe = pipeline("document-question-answering", model="impira/layoutlm-document-qa")
 
+
+# Q&A form
 with st.form('myform'):
     query_text = st.text_input('Enter your question:', '')
     submitted = st.form_submit_button('Submit')
@@ -45,7 +97,7 @@ with st.form('myform'):
 
 
 
-
+# CSV Data Analysis
 if uploaded_file is not None:
     # Read the CSV file
     df = pd.read_csv(uploaded_file)
